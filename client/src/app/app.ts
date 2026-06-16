eef0cf403ea0942cd6129fa5efce7eb5637262ce
Feng <feng.zhu1980@gmail.com>
2026-06-16 12:05:21 +1200

    feat: enhance application structure with routing, guards, and initialization services

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,47 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
-import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
+import { Router, RouterOutlet } from "@angular/router";
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'Dating app';
-  protected members = signal<User[]>([])
+export class App {
+  protected router = inject(Router);
 
-  async ngOnInit() {
+  // async ngOnInit() {
     // this.http.get('https://localhost:5001/api/members').subscribe({
     //   next: response => this.members.set(response),
     //   error: error => console.log(error),
     //   complete: () => console.log('Completed the http request')
     // })
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
+    // this.members.set(await this.getMembers());
+  // }
 
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
+  // async getMembers() {
+  //   try {
+  //     return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,27 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
-  protected creds: any = {}
+  private toast = inject(ToastService);
+  private router = inject(Router);
+  protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: result => {
-        this.creds = [];
+      next: () => {
+        this.creds = {};
+        this.router.navigate(['/members']);
+        this.toast.success('Logged in successfully');
       },
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        this.toast.error(error.error);
+      }
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigate(['/']);
   }
 }
