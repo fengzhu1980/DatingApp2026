79f2b28e3fa4c01cc01cd4074aca62165fc119ad
Feng <feng.zhu1980@gmail.com>
2026-06-17 22:42:04 +1200

    feat: implement member profile update functionality with form handling and loading indicators

---
+++ client/src/core/guards/prevent-unsaved-changes-guard.ts
@@ -0,0 +1,15 @@
+import { CanDeactivateFn } from '@angular/router';
+import { MemberProfile } from '../../features/members/member-profile/member-profile';
+
+export const preventUnsavedChangesGuard: CanDeactivateFn<MemberProfile> = (
+  component,
+  currentRoute,
+  currentState,
+  nextState,
+) => {
+  if (component.editForm?.dirty) {
+    return confirm('Are you sure');
+  }
+
+  return true;
+};
